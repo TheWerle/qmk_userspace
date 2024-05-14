7db13717176a2ab38e9e276426ce7c9f585a0e84
TheWerle <159328578+TheWerle@users.noreply.github.com>
2024-05-14 16:52:10 -0400

    fixed json / layout mismatch for keymaps

--- keyboards/sofle_pico/keymaps/TheWerle/keymap.c
+++ keyboards/sofle_pico/keymaps/TheWerle/keymap.c
@@ -14,20 +14,10 @@ enum sofle_layers {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _ADJUST//,
-	//_MOUSE
+    _ADJUST,
+	_MOUSE
 };
 
-enum custom_keycodes {
-    KC_QWERTY = SAFE_RANGE,
-	SNIPE,
-    EXIT_MOUSE,
-    KC_PRVWD,
-    KC_NXTWD,
-    KC_LSTRT,
-    KC_LEND,
-    KC_DLINE
-};
 
 	const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -37,11 +27,11 @@ enum custom_keycodes {
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
 	 * | TAB |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P   |   -	|
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-	 * |LSHFT|   A  |   S  |   D  |   F  |   G   -------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+	 * |LSHFT|   A  |   S  |   D  |   F  |   G    -------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
 	 * |------+------+------+------+------+------|  GESC |    |       |------+------+------+------+------+------|
-	 * |LCTR|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \ 	|
+	 * |LCTR|   Z  |   X  |   C  |   V  |   B  	 |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \ 	|
 	 * --------------------+++-------------------/       /     \      \-----------------------------------------'
-	 *            | LGUI | LAlt | LCTR | Enter  / Space /       \RAISE \  |Enter |   [   |   ] | RGUI |
+	 *            | LGUI | LAlt | LCTR | LOWER  / Space /       \Enter \  |RAISE |   [   |   ] | RGUI |
 	 */
 
 	[_QWERTY] = LAYOUT(
@@ -55,21 +45,21 @@ enum custom_keycodes {
 	 * ,-----------------------------------------.                    ,-----------------------------------------.
 	 * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-	 * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+	 * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   \  | F12  |
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-	 * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+	 * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   6  |   7  |   8  |   *  |   |  |
 	 * |------+------+------+------+------+------|RGB_TOG|    |       |------+------+------+------+------+------|
-	 * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Shift|
+	 * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   -  | Shift|
 	 * `-----------------------------------------/       /     \      \-----------------------------------------'
-	 *            | LGUI | LAlt | LCTR |Space | / LOWER /       \RAISE \  |Enter | RCTR | RAlt | RGUI |
+	 *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Enter \  |RAISE | 0    | .     |
 	 *            |      |      |      |      |/       /         \      \ |      |      |      |      |
 	 */
 	[_LOWER] = LAYOUT(
-	  QK_GESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-	  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-	  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-	  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, _______, _______,
-	  _______, _______, _______, _______, _______, RGB_TOG, _______, _______, _______, _______, _______, _______
+	  QK_GESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,   KC_F7,  KC_F8, KC_F9,  KC_F10,  KC_F11,
+	  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_NUM,  KC_P7,  KC_P8, KC_P9,  KC_PSLS, KC_F12,
+	  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_HOME, KC_P4,  KC_P5, KC_P6,  KC_PAST, KC_PIPE,
+	  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_END,  KC_P1,  KC_P2, KC_P3,  KC_PMNS, _______,
+	  _______, _______, _______, _______, _______, RGB_TOG, _______,_______,_______,KC_P0,  KC_PDOT, KC_PPLS
 	),
 	/* RAISE
 	 * ,----------------------------------------.                    ,-----------------------------------------.
@@ -78,16 +68,16 @@ enum custom_keycodes {
 	 * | Esc  | Ins  | Pscr | Menu |      |DB_TOGG|                    |      | PWrd |  Up  | NWrd | DLine| Bspc |
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
 	 * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------|      | Left | Down | Right |  Del | Bspc |
-	 * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
+	 * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
 	 * |Shift | Undo |  Cut | Copy | Paste|      |-------|    |-------|      | LStr |      | LEnd |      | Shift|
 	 * `-----------------------------------------/       /     \      \-----------------------------------------'
-	 *            | LGUI | LAlt | LCTR |Space | / LOWER /       \RAISE \  |Enter | RCTR | RAlt | RGUI |
+	 *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
 	 *            |      |      |      |      |/       /         \      \ |      |      |      |      |
 	 */
     [_RAISE] = LAYOUT(
     _______, _______, _______, _______, RGB_SAI, RGB_SAD,RGB_M_SW, RGB_M_SN,DB_TOGG, RGB_MOD,RGB_RMOD, _______,
-    _______,  KC_INS, KC_PSCR, KC_APP , RGB_VAI, RGB_TOG, KC_PGUP, _______,   KC_UP, _______, _______, KC_BSPC,
-    _______, KC_LALT, KC_LCTL, KC_LSFT, RGB_VAD, KC_CAPS, KC_PGDN,  KC_LEFT,KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+    _______,  KC_INS, KC_PSCR, KC_APP , RGB_VAI, RGB_TOG, KC_PGUP, _______, KC_UP,   _______, _______, KC_BSPC,
+    _______, KC_LALT, KC_LCTL, KC_LSFT, RGB_VAD, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
     _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX, _______,
     XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -101,7 +91,7 @@ enum custom_keycodes {
 	 * |------+------+------+------+------+------| MU TOG|    |       |------+------+------+------+------+------|
 	 * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
 	 * `-----------------------------------------/       /     \      \-----------------------------------------'
-	 *            | LGUI | LAlt | LCTR |Space | / LOWER /       \RAISE \  |Enter | RCTR | RAlt | RGUI |
+	 *            | LGUI | LAlt | LCTR | LOWER| / Space /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
 	 *            |      |      |      |      |/       /         \      \ |      |      |      |      |
 	 */
 	  [_ADJUST] = LAYOUT(
@@ -110,14 +100,23 @@ enum custom_keycodes {
 	  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
 	  QK_REBOOT, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT,
       XXXXXXX, XXXXXXX, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______
+	  ),
+	  [_MOUSE] = LAYOUT(
+	   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+	   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       _______, _______, _______, _______, KC_BTN1, _______, _______, KC_BTN2, KC_BTN3, _______, _______, _______
 	  )
+
 	};
 	#ifdef ENCODER_MAP_ENABLE
 		const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 		   [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-		   [1] = { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD) },
-		   [3] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP ) },
-		   [2] = { ENCODER_CCW_CW(AU_PREV, AU_NEXT) }
+		   [1] = { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD)},
+		   [2] = { ENCODER_CCW_CW(AU_PREV, AU_NEXT) },
+           [3] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP )},
+		   [4] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX )}
 		  };
 	#endif
 
@@ -126,7 +125,7 @@ enum custom_keycodes {
 	  debug_enable=true;
 	  debug_matrix=true;
 	  //debug_keyboard=true;
-	  //debug_mouse=true;
+	  debug_mouse=true;
 	}
 
     // Default timeout for displaying logo on boot.
@@ -324,6 +323,9 @@ enum custom_keycodes {
 			case _ADJUST:
 				oled_write_P(PSTR("ADJUST "), false);
 				break;
+            case _MOUSE:
+				oled_write_P(PSTR("MOUSE  "), false);
+			break;
             default:
                 oled_write_P(PSTR("UNK    "), false);
                 break;
@@ -383,6 +385,11 @@ enum custom_keycodes {
 
 
     #endif
+
+void pointing_device_init_user(void) {
+    set_auto_mouse_layer(_MOUSE); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
